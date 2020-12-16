9292356eabc9536022c8da39fa101c3c3b8e373c
Johan Bency <jbency@overstock.com>
2020-12-16 15:37:59 +0000

    Adding missing requestmapping annotation, auto generated contract tests working with dummy response

--- src/main/java/com/johan/project/filesearchservice/web/FileSearchController.java
+++ src/main/java/com/johan/project/filesearchservice/web/FileSearchController.java
@@ -1,12 +1,10 @@
 package com.johan.project.filesearchservice.web;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping
+@RestController
 public class FileSearchController {
 
   @GetMapping(path = "/documents")
