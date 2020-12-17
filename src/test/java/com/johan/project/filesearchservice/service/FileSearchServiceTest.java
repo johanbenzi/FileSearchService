6f55fb9ab81b7de32c0c8c0e5a44372a3ae479c8
Johan Bency <jbency@overstock.com>
2020-12-17 16:08:17 +0000

    Adding more UTs at service layer to test edge cases

--- src/test/java/com/johan/project/filesearchservice/service/FileSearchServiceTest.java
+++ src/test/java/com/johan/project/filesearchservice/service/FileSearchServiceTest.java
@@ -1,15 +1,19 @@
 package com.johan.project.filesearchservice.service;
 
+import com.johan.project.filesearchservice.exceptions.KeywordNotFoundInDocumentException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("FileSearchService Unit Tests")
 class FileSearchServiceTest {
+
+  private static final String FILE_CONTENT = "This is a new sample text we are using to test the class";
 
   private static final String KEYWORD = "sample";
 
@@ -18,8 +22,37 @@ class FileSearchServiceTest {
 
   @Test
   void searchForKeyword_happyPath() {
-    final String searchResponse = cut.searchForKeyword(KEYWORD);
+    ReflectionTestUtils.setField(cut, "fileContent", FILE_CONTENT);
 
-    Assertions.assertNotNull(searchResponse);
+    Assertions.assertEquals(" is a new sample text we a", cut.searchForKeyword(KEYWORD));
+  }
+
+  @Test
+  void searchForKeyword_lessCharactersAvailableInLeft() {
+    ReflectionTestUtils.setField(cut, "fileContent", FILE_CONTENT.substring(10));
+
+    Assertions.assertEquals("new sample text we a", cut.searchForKeyword(KEYWORD));
+  }
+
+  @Test
+  void searchForKeyword_lessCharactersAvailableInRight() {
+    ReflectionTestUtils.setField(cut, "fileContent", FILE_CONTENT.substring(0, 25));
+
+    Assertions.assertEquals(" is a new sample text", cut.searchForKeyword(KEYWORD));
+  }
+
+  @Test
+  void searchForKeyword_noKeywordFound_throwsException() {
+    ReflectionTestUtils.setField(cut, "fileContent", FILE_CONTENT);
+
+    Assertions.assertThrows(KeywordNotFoundInDocumentException.class,
+      () -> cut.searchForKeyword("a text that doesn't exist in content"));
+  }
+
+  @Test
+  void searchForKeyword_noExtraText() {
+    ReflectionTestUtils.setField(cut, "fileContent", KEYWORD);
+
+    Assertions.assertEquals(KEYWORD, cut.searchForKeyword(KEYWORD));
   }
 }
